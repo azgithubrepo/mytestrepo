4d0e0864b0418aea2b9cb698eac0780d32e15773
ajju <ajju@test.com>
2018-08-10 10:48:31 +0530

    Some chnages to test  sonar

--- src/Test.java
+++ src/Test.java
@@ -4,7 +4,7 @@ class Test{
 	public static void main(String args[]){
 		System.out.print("hello");
 		for(;;){
-			alert("Alert should not be used");
+			System.out.print("Alert should not be used");
 		}
 }
 }
